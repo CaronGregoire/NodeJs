7e6eeddcfca0984c667198e5edabef3b4a564e8e
CaronGregoire <gregoirecaron33@gmail.com>
2022-05-06 14:51:16 +0200

    add route get put and delete for object

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const Thing = require('./models/Thing');
 mongoose.connect(process.env.DATA_CONNECTION,
 { useNewUrlParser: true,
   useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
   
 const app = express();
 
@@ -26,15 +26,33 @@ app.post('/api/stuff', (req, res, next) => {
     ...req.body
     });
     thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+      .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+      .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+  Thing.find() 
+    .then(things => res.status(200).json( things ))
     .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find() 
-    .then(things => res.status(200).json( things ))
-    .catch(error => res.status(404).json({ error }));
-  });
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
 
