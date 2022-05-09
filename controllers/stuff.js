e1afe01ed23ce37ad6da0172197366263708d441
CaronGregoire <gregoirecaron33@gmail.com>
2022-05-09 11:08:31 +0200

    add multer and modif route

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,9 +1,12 @@
 const Thing = require('../models/Thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
-    delete req.body._id;
+    const thingObject = JSON.parse(req.body.thing);
+    delete thingObject._id;
     const thing = new Thing({
-    ...req.body
+    ...thingObject,
+    imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`, 
     });
     thing.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
@@ -11,28 +14,28 @@ exports.createThing = (req, res, next) => {
 }
 
 exports.modifyThing = (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    const thingObject = req.file ?
+    {
+        ...JSON.parse(req.body.thing),
+        imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`,     
+    } : { ...req.body };
+    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !' }))
         .catch(error => res.status(400).json({ error }));
 }
 
 exports.deleteThing = (req, res, next) => {
-    Thing.findOne({_id: req.param.id}) .then(
-        (thing) => {
-            if (!thing) {
-                res.status(404).json({
-                    error: new Error('objet non trouvé')
-                });
-            }
-            if (thing.userId !== req.auth.userId) {
-                res.status(401).json({
-                    error: new Error('Requete non authorisée!')
-                });
-            }
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })};
+    Thing.findOne({_id: req.param.id}) 
+    .then(thing => {
+        const filename = thing.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Thing.deleteOne({ _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+                .catch(error => res.status(400).json({ error }));
+        });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
 
 exports.getOneThing = (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const stuffController = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, stuffController.createThing);
-router.put('/:id', auth, stuffController.modifyThing);
+router.post('/', auth, multer, stuffController.createThing);
+router.put('/:id', auth, multer, stuffController.modifyThing);
 router.delete('/:id', auth, stuffController.deleteThing);
 router.get('/:id', auth, stuffController.getOneThing);
 router.get('/', auth, stuffController.getAllThings);
